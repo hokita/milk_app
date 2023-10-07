a9b7280ba68c282c5106d9ec67601cf821e18229
hokita <hideee.0202@gmail.com>
2023-10-07 20:48:13 +0900

    Initial Commit

---
+++ Milk/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Milk
+//
+//  Created by Hidetaka Okita on 2023/10/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Milk/MilkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MilkApp.swift
+//  Milk
+//
+//  Created by Hidetaka Okita on 2023/10/07.
+//
+
+import SwiftUI
+
+@main
+struct MilkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
